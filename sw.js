11143ebe1dd6ede66ca06e151c13510014f57878
wahabnellikuth-hub <wahabnellikuth@gmail.com>
2026-06-29 19:45:17 +0530

    Fix SW cache to ensure updates reach all devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'food-service-v1';
+const CACHE_NAME = 'food-service-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -9,9 +9,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
